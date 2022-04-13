75013b0a618298abc55960758eb65eda9fe117e6
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-04-13 00:26:11 -0700

    commit tree a73e159ffa7fc1dd13231859ec2e2e3fcaf50724

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arrayObj, occasion) {
+    const messages = [];
+    
+    for (let i = 0; i < arrayObj.length; i++) {
+        messages.push(`Thank you, ${arrayObj[i]}, for the wonderful ${occasion} gift!`);
+    }
+
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number -= 1;
+    }
+}
